0d3c54e6416713c8a1966f5a0b1cea1090103335
shubhamselenum <sg165191@gmail.com>
2019-10-04 16:22:04 +0530

    Last in First Commit

---
+++ Git/src/Git_Demo/Git_Test.java
@@ -0,0 +1,17 @@
+package Git_Demo;
+
+public class Git_Test {
+	
+	static class git {
+		 public static void main(String[] args) {
+			System.out.println("I am in main method");
+		}
+	
+		 
+		 private void syso() {
+			System.out.println("I am in Private method");
+		}
+		 
+	 }
+
+}
